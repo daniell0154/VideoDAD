d998898b328890fd7b868af5b3b664f9b1d5a319
daniell0154 <danieldejesusferreira01@gmail.com>
2025-08-29 11:30:48 -0300

    Adicionando funcionalidade de passar o video

--- final/app.js
+++ final/app.js
@@ -1,5 +1,10 @@
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
+const btnPular = document.querySelector(".pular-btn")
+const videos = ["./media/video.mp4", "./media/video2.mp4", "./media/video3.mp4"]
+const source = document.querySelector("#src")
+
+let index = 0
 
 btn.addEventListener("click", function () {
   if (!btn.classList.contains("slide")) {
@@ -17,3 +22,11 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener("load", function () {
   preloader.classList.add("hide-preloader");
 });
+
+btnPular.addEventListener("click", () => {
+  index = (index + 1) % videos.length
+  source.src = videos[index]
+  btn.classList.remove("slide")
+  video.load()
+  video.play()
+})
