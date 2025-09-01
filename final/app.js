05d4a50479420b2b3308996a89a61befa3c24bac
daniell0154 <danieldejesusferreira01@gmail.com>
2025-09-01 07:08:08 -0300

    Adicionando video de transicao

--- final/app.js
+++ final/app.js
@@ -3,7 +3,9 @@ const video = document.querySelector(".video-container");
 const btnPular = document.querySelector(".pular-btn")
 const videos = ["./media/video.mp4", "./media/video2.mp4", "./media/video3.mp4"]
 const source = document.querySelector("#src")
-const header = document.querySelector("header ")
+const header = document.querySelector("header")
+const titulo = document.querySelector("h1")
+const transicaoVideo = document.querySelector(".videoTransicao");
 
 let index = 0
 
@@ -12,11 +14,14 @@ btn.addEventListener("click", function () {
     btn.classList.add("slide");
     video.pause();
     video.controls = true
-    
+    header.classList.add("no-bg")
+    titulo.textContent = ""
   } else {
     btn.classList.remove("slide");
     video.play();
     video.controls = false
+    header.classList.remove("no-bg");
+    titulo.textContent = "PROJETO DE VÍDEO!!"
   }
 });
 
@@ -28,9 +33,18 @@ window.addEventListener("load", function () {
 });
 
 btnPular.addEventListener("click", () => {
-  index = (index + 1) % videos.length
-  source.src = videos[index]
-  btn.classList.remove("slide")
-  video.load()
-  video.play()
-})
+  // mostra e toca o vídeo de transição
+  transicaoVideo.style.display = "block";
+  transicaoVideo.play();
+
+  transicaoVideo.onended = () => {
+    transicaoVideo.style.display = "none"; // esconde a transição
+
+    // troca para o próximo vídeo
+    index = (index + 1) % videos.length;
+    source.src = videos[index];
+    btn.classList.remove("slide");
+    video.load();
+    video.play();
+  };
+});
