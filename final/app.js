de41cca27ce98e4f96bd6472fa90950e30102fc6
daniell0154 <danieldejesusferreira01@gmail.com>
2025-09-01 15:43:01 -0300

    Adicionando tema ao projeto e corrigindo diversos erros

--- final/app.js
+++ final/app.js
@@ -6,6 +6,8 @@ const source = document.querySelector("#src")
 const header = document.querySelector("header")
 const titulo = document.querySelector("h1")
 const transicaoVideo = document.querySelector(".videoTransicao");
+const avancar = document.querySelector(".avancar");
+const voltar = document.querySelector(".voltar");
 
 let index = 0
 
@@ -13,38 +15,81 @@ btn.addEventListener("click", function () {
   if (!btn.classList.contains("slide")) {
     btn.classList.add("slide");
     video.pause();
-    video.controls = true
     header.classList.add("no-bg")
     titulo.textContent = ""
+
   } else {
     btn.classList.remove("slide");
     video.play();
-    video.controls = false
     header.classList.remove("no-bg");
-    titulo.textContent = "PROJETO DE VÍDEO!!"
+    titulo.textContent = "Os melhores trailers da humanidade!"
   }
 });
 
 // preloader
-const preloader = document.querySelector(".preloader");
+// const preloader = document.querySelector(".preloader");
 
 window.addEventListener("load", function () {
   preloader.classList.add("hide-preloader");
 });
 
-btnPular.addEventListener("click", () => {
-  // mostra e toca o vídeo de transição
+// Botão pular (executa transição e troca vídeo)
+btnPular.addEventListener("click", trocarVideo)
+
+// Voltar 10 segundos (ou início se < 10s)
+voltar.addEventListener("click", function() {
+  if (video.currentTime < 10){
+    video.currentTime = 0
+  }
+  else {
+    video.currentTime -= 10
+  }
+})
+
+// Avançar 10 segundos
+avancar.addEventListener("click", function() {
+  video.currentTime += 10
+})
+
+// Atalhos de teclado (espaço, setas)
+document.addEventListener("keydown", (event) => {
+  const tecla = event.key.toLowerCase();
+
+  if (tecla === " ") {
+    event.preventDefault();
+    btn.click()
+  } else if (tecla === "arrowright") {
+    avancar.click()
+
+  } else if (tecla === "arrowleft") { 
+    voltar.click()
+
+  }
+});
+
+// Função para trocar de vídeo
+function trocarVideo() {
+  voltar.style.display = "none"
+  avancar.style.display = "none"
+  btn.style.display = "none"
+  btnPular.style.display = "none"
+
   transicaoVideo.style.display = "block";
   transicaoVideo.play();
+  transicaoVideo.style.zIndex = "0"
 
   transicaoVideo.onended = () => {
-    transicaoVideo.style.display = "none"; // esconde a transição
+    transicaoVideo.style.display = "none";
 
-    // troca para o próximo vídeo
+    voltar.style.display = "flex"
+    avancar.style.display = "flex"
+    btn.style.display = "flex"
+    btnPular.style.display = "flex"
+
     index = (index + 1) % videos.length;
     source.src = videos[index];
     btn.classList.remove("slide");
     video.load();
     video.play();
   };
-});
+}
